4946d22aebdd3aff0971dfc4db7db11733f1ea17
David Smith <davidsmi@microsoft.com>
2020-08-12 09:44:08 -0700

    wait for train completion

--- train-model.R
+++ train-model.R
@@ -2,6 +2,21 @@ library(azuremlsdk)
 
 ws <- load_workspace_from_config()
 
+## TODO: Get compute cluster from prior step
+cluster_name <- "rcluster"
+compute_target <- get_compute(ws, cluster_name = cluster_name)
+if (is.null(compute_target)) {
+  vm_size <- "STANDARD_D2_V2" 
+  compute_target <- create_aml_compute(workspace = ws,
+                                       cluster_name = cluster_name,
+                                       vm_size = vm_size,
+                                       min_nodes = 0,
+                                       max_nodes = 2)
+
+  wait_for_provisioning_completion(compute_target, show_output = TRUE)
+}
+
+
 ds <- get_default_datastore(ws)
 target_path <- "accidentdata"
 
